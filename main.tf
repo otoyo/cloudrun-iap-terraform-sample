1f04ebee06955dd3a302bcfb28f952734e5feea3
otoyo <hiroki.t.1988@gmail.com>
2022-01-03 15:44:04 +0900

    Define Cloud Run

--- main.tf
+++ main.tf
@@ -38,3 +38,71 @@ resource "google_sql_user" "rails7" {
   host     = "%"
   password = var.db_password
 }
+
+#
+# Cloud Run
+#
+resource "google_cloud_run_service" "rails7-cloudrun-sample" {
+  name     = "rails7-cloudrun-sample"
+  location = "asia-northeast1"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/cloudrun-terraform-sample/rails7-cloudrun-sample"
+        env {
+          name  = "RAILS_ENV"
+          value = "production"
+        }
+        env {
+          name  = "RAILS_LOG_TO_STDOUT"
+          value = "1"
+        }
+        env {
+          name  = "RAILS_SERVE_STATIC_FILES"
+          value = "1"
+        }
+        env {
+          name  = "RAILS_MASTER_KEY"
+          value_from {
+            secret_key_ref {
+              name = "rails-master-key"
+              key  = "latest"
+            }
+          }
+        }
+      }
+    }
+
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale"      = "1000"
+        "run.googleapis.com/cloudsql-instances" = google_sql_database_instance.rails7.connection_name
+        "run.googleapis.com/client-name"        = "terraform"
+      }
+    }
+  }
+
+  metadata {
+    annotations = {
+      "run.googleapis.com/launch-stage" = "BETA"
+    }
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.rails7-cloudrun-sample.location
+  project     = google_cloud_run_service.rails7-cloudrun-sample.project
+  service     = google_cloud_run_service.rails7-cloudrun-sample.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
