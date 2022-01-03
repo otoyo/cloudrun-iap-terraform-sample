02aa73b1ff912abfe2c5cae512f28e9fa20d60e9
otoyo <hiroki.t.1988@gmail.com>
2022-01-03 14:29:26 +0900

    Define Cloud SQL

--- main.tf
+++ main.tf
@@ -3,3 +3,38 @@ provider "google" {
   region  = "asia-northeast1"
   zone    = "asia-northeast1-b"
 }
+
+#
+# Variables
+#
+variable "db_password" {
+  description = "Database user password"
+  type        = string
+}
+
+#
+# Cloud SQL
+#
+resource "google_sql_database_instance" "rails7" {
+  name             = "rails7"
+  database_version = "MYSQL_5_7"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection  = "true"
+}
+
+resource "google_sql_database" "rails7_cloudrun_sample" {
+  name      = "rails7_cloudrun_sample"
+  instance  = google_sql_database_instance.rails7.name
+  charset   = "utf8mb4"
+  collation = "utf8mb4_bin"
+}
+
+resource "google_sql_user" "rails7" {
+  name     = "rails7"
+  instance = google_sql_database_instance.rails7.name
+  host     = "%"
+  password = var.db_password
+}
