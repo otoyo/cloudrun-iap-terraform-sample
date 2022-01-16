7374f12e21b8976f9797162c9d8b0bb89deabe5c
otoyo <hiroki.t.1988@gmail.com>
2022-01-16 15:57:36 +0900

    Add Cloud Load Balancing

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "cloudrun-iap-terraform-sample"
+  project = "rails7-cloudrun-iap-sample"
   region  = "asia-northeast1"
   zone    = "asia-northeast1-b"
 }
@@ -9,6 +9,16 @@ provider "google" {
 #
 variable "db_password" {
   description = "Database user password"
+  type        = string
+}
+
+variable "iap_email" {
+  description = "IAP user email"
+  type        = string
+}
+
+variable "domain" {
+  description = "Domain"
   type        = string
 }
 
@@ -49,7 +59,7 @@ resource "google_cloud_run_service" "rails7-cloudrun-iap-sample" {
   template {
     spec {
       containers {
-        image = "gcr.io/cloudrun-terraform-sample/rails7-cloudrun-iap-sample"
+        image = "gcr.io/rails7-cloudrun-iap-sample/rails7-cloudrun-iap-sample"
         env {
           name  = "RAILS_ENV"
           value = "production"
@@ -83,26 +93,82 @@ resource "google_cloud_run_service" "rails7-cloudrun-iap-sample" {
     }
   }
 
-  metadata {
-    annotations = {
-      "run.googleapis.com/launch-stage" = "BETA"
-    }
+  autogenerate_revision_name = true
+}
+
+#
+# Cloud Load Balancing
+#
+resource "google_compute_global_address" "rails7-cloudrun-iap-sample" {
+  name = "rails7-cloudrun-iap-sample-global-address"
+}
+
+resource "google_compute_managed_ssl_certificate" "rails7-cloudrun-iap-sample" {
+  name = "rails7-cloudrun-iap-sample-cert"
+
+  managed {
+    domains = [var.domain]
   }
 }
 
-data "google_iam_policy" "noauth" {
-  binding {
-    role = "roles/run.invoker"
-    members = [
-      "allUsers",
-    ]
+resource "google_compute_region_network_endpoint_group" "rails7-cloudrun-iap-sample" {
+  name                  = "rails7-cloudrun-iap-sample-neg"
+  region                = "asia-northeast1"
+  network_endpoint_type = "SERVERLESS"
+  cloud_run {
+    service = google_cloud_run_service.rails7-cloudrun-iap-sample.name
   }
 }
 
-resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.rails7-cloudrun-iap-sample.location
-  project     = google_cloud_run_service.rails7-cloudrun-iap-sample.project
-  service     = google_cloud_run_service.rails7-cloudrun-iap-sample.name
+resource "google_compute_backend_service" "rails7-cloudrun-iap-sample" {
+  name      = "rails7-cloudrun-iap-sample-backend"
 
-  policy_data = data.google_iam_policy.noauth.policy_data
+  protocol  = "HTTP"
+  port_name = "http"
+  timeout_sec = 30
+
+  backend {
+    group = google_compute_region_network_endpoint_group.rails7-cloudrun-iap-sample.id
+  }
+}
+
+resource "google_iap_web_backend_service_iam_binding" "rails7-cloudrun-iap-sample" {
+  web_backend_service = google_compute_backend_service.rails7-cloudrun-iap-sample.name
+  role = "roles/iap.httpsResourceAccessor"
+  members = [
+    "user:${var.iap_email}",
+  ]
+}
+
+resource "google_compute_url_map" "rails7-cloudrun-iap-sample" {
+  name            = "rails7-cloudrun-iap-sample-urlmap"
+
+  default_service = google_compute_backend_service.rails7-cloudrun-iap-sample.id
+}
+
+resource "google_compute_target_https_proxy" "rails7-cloudrun-iap-sample" {
+  name   = "rails7-cloudrun-iap-sample-https-proxy"
+
+  url_map          = google_compute_url_map.rails7-cloudrun-iap-sample.id
+  ssl_certificates = [
+    google_compute_managed_ssl_certificate.rails7-cloudrun-iap-sample.id
+  ]
+}
+
+resource "google_compute_global_forwarding_rule" "rails7-cloudrun-iap-sample" {
+  name   = "rails7-cloudrun-iap-sample-forwarding-rule"
+
+  target = google_compute_target_https_proxy.rails7-cloudrun-iap-sample.id
+  port_range = "443"
+  ip_address = google_compute_global_address.rails7-cloudrun-iap-sample.address
+}
+
+# Outputs
+
+output "load_balancer_ip" {
+  value = google_compute_global_address.rails7-cloudrun-iap-sample.address
+}
+
+output "cloud_run_url" {
+  value = element(google_cloud_run_service.rails7-cloudrun-iap-sample.status, 0).url
 }
